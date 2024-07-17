4eb1f3f6bc0e294f2fce06d0b8809ffa7ad2f20b
cheshiry <agnesdacunha02@gmail.com>
2024-07-17 09:59:25 -0300

    xp

---
+++ desafio_Dio.js
@@ -0,0 +1,20 @@
+let heroi = [
+    nome = "titã", 
+    xp = 7000,
+    genero = "M",
+    classe = "guerreiro"
+]
+
+/*
+xp <= 1000 = ferro
+xp entre 1001 e 2000 = bronze
+xp entre 2001 e 5000 = prata
+xp entre 6001 e 7000 = ouro
+xp entre 7001 e 8000 = platina
+xp entre 8001 e 9000 = ascentente
+xp entre 9001 e 10000 = imortal
+xp >= 10001 = radiante
+
+
+console.log(heroi);
+*/
