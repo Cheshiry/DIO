a7b48b414c80352ad62fd7bd0bf75cdbb5c3afc0
cheshiry <agnesdacunha02@gmail.com>
2024-07-17 22:55:58 -0300

    ok

--- desafio_Dio.js
+++ desafio_Dio.js
@@ -1,46 +1,31 @@
-/*let heroi = [*/
+//Desafio 
   let  nome = "titã"; 
     let rank = "";
     let xp = 11000;
     let genero = "M";
     let classe = "guerreiro";
 
-
-
 if (xp <= 1000)  {
    rank = "ferro";
-}else if 
 
-(xp >=1001 && xp <= 2000){
-
+}else if (xp >=1001 && xp <= 2000){
 rank = "bronze"
 
-}else if
-(xp >= 2001 && xp <= 5000) {
-
+}else if (xp >= 2001 && xp <= 5000) {
 rank = "prata"
 
-}else if
-
-(xp >= 6001 && xp <= 7000) {
-
+}else if (xp >= 6001 && xp <= 7000) {
  rank = "ouro"
 
-}else if
-
-(xp >= 7001 && xp <= 8000) {
+}else if (xp >= 7001 && xp <= 8000) {
 
     rank = "platina"
 
-}else if
-
-(xp >= 8001 && xp <= 9000) {
+}else if (xp >= 8001 && xp <= 9000) {
 
     rank = "ascentente"
 
-}else if
-
-(xp >=9001 && xp <=10000) {
+}else if (xp >=9001 && xp <=10000) {
 
     rank = "imortal"
 
