93bb9ee0b3d6dd219d334f3dea3917dac3a48e16
cheshiry <agnesdacunha02@gmail.com>
2024-07-17 23:01:11 -0300

    ok alinhado

--- desafio_Dio.js
+++ desafio_Dio.js
@@ -1,36 +1,32 @@
 //Desafio 
-  let  nome = "titã"; 
-    let rank = "";
-    let xp = 11000;
-    let genero = "M";
-    let classe = "guerreiro";
+let  nome = "titã"; 
+let rank = "";
+let xp = 11000;
+let genero = "M";
+let classe = "guerreiro";
 
 if (xp <= 1000)  {
    rank = "ferro";
 
 }else if (xp >=1001 && xp <= 2000){
-rank = "bronze"
+    rank = "bronze"
 
 }else if (xp >= 2001 && xp <= 5000) {
-rank = "prata"
+    rank = "prata"
 
 }else if (xp >= 6001 && xp <= 7000) {
- rank = "ouro"
+    rank = "ouro"
 
 }else if (xp >= 7001 && xp <= 8000) {
-
     rank = "platina"
 
 }else if (xp >= 8001 && xp <= 9000) {
-
     rank = "ascentente"
 
 }else if (xp >=9001 && xp <=10000) {
-
     rank = "imortal"
 
 }else{
-
     rank = "radiante"
 
 }
