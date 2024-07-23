70246008441f715fc6dc600740ac7032c1e93986
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-07-23 11:04:27 -0300

    Atualizar o desafioRankeada.js
    construção dos if

--- desafioRankeada.js
+++ desafioRankeada.js
@@ -37,14 +37,27 @@ function rankeada(won, lose){
 
 function nivel(){
 
-Se vitórias for menor do que 10 = Ferro
-Se vitórias for entre 11 e 20 = Bronze
-Se vitórias for entre 21 e 50 = Prata
-Se vitórias for entre 51 e 80 = Ouro
-Se vitórias for entre 81 e 90 = Diamante
-Se vitórias for entre 91 e 100= Lendário
-Se vitórias for maior ou igual a 101 = Imortal
-
+if vitórias for menor do que 10 {
+Ferro
+}
+if vitórias for entre 11 e 20 {
+Bronze
+}
+if vitórias for entre 21 e 50 {
+Prata
+}
+if vitórias for entre 51 e 80 {
+Ouro
+}
+if vitórias for entre 81 e 90 {
+Diamante
+}
+if vitórias for entre 91 e 100{
+Lendário
+}
+if vitórias for maior ou igual a 101 {
+Imortal
+}
 }
 mensagem
 `O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`
