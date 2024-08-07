0e4ca0bf8b4beef182e8a67066547003870c1df7
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-08-07 16:05:02 -0300

    Create desafioFinal.js
    Class criada

---
+++ desafioFinal.js
@@ -0,0 +1,21 @@
+let heroi = "Belmont";
+let idade = 35;
+let tipo = "Guerreiro";
+
+class Heroi{
+  constructor(heroi, idade){
+    this.heroi = heroi;
+    this.idade = idade;
+
+    console.log(`O ${this.heroi} tem ${idade}`);
+  }
+}
+
+class Ataque{
+  constructor(tipo){
+    switch ()
+      case
+  }
+}
+
+let heroiInformacoes = new Heroi(heroi, idade)
